40f10210eeff7c19e83958d737a79970e19e1fe2
Katherine928 <katherine71317@gmail.com>
2023-05-18 13:26:35 -0400

    project set up

---
+++ src/main/java/net/katherine/graphqlmovieapp/GraphqlMovieAppApplication.java
@@ -0,0 +1,13 @@
+package net.katherine.graphqlmovieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlMovieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlMovieAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/katherine/graphqlmovieapp/GraphqlMovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.katherine.graphqlmovieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlMovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
