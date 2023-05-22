4ed93ff267b02f62b35ec8ed6e6cb72002fed656
Katherine928 <katherine71317@gmail.com>
2023-05-22 11:24:07 -0400

    Movies Api

--- src/main/java/net/katherine/graphqlmovieapp/controller/MovieController.java
+++ src/main/java/net/katherine/graphqlmovieapp/controller/MovieController.java
@@ -3,11 +3,17 @@ package net.katherine.graphqlmovieapp.controller;
 
 import net.katherine.graphqlmovieapp.client.MovieClient;
 import net.katherine.graphqlmovieapp.dto.Customer;
+import net.katherine.graphqlmovieapp.dto.Genre;
 import net.katherine.graphqlmovieapp.dto.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Controller
 public class MovieController {
@@ -23,5 +29,15 @@ public class MovieController {
     @SchemaMapping(typeName = "UserProfile")
     public Flux<Movie> recommended(Customer customer){
         return client.moviesByGenre(customer.getFavoriteGenre());
+    }
+
+    @QueryMapping
+    public Mono<Movie> movieDetails(@Argument Integer id) {
+        return client.getMoviesByIds(List.of(id)).next();
+    }
+
+    @QueryMapping
+    public Flux<Movie> moviesByGenre(@Argument Genre genre) {
+        return client.moviesByGenre(genre);
     }
 }
--- src/main/java/net/katherine/graphqlmovieapp/controller/ReviewController.java
+++ src/main/java/net/katherine/graphqlmovieapp/controller/ReviewController.java
@@ -2,12 +2,22 @@ package net.katherine.graphqlmovieapp.controller;
 
 
 import net.katherine.graphqlmovieapp.client.ReviewClient;
+import net.katherine.graphqlmovieapp.dto.Movie;
+import net.katherine.graphqlmovieapp.dto.Review;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 
 @Controller
 public class ReviewController {
 
     @Autowired
     private ReviewClient client;
+
+
+    @SchemaMapping(typeName = "MovieDetails")
+    public Flux<Review> reviews(Movie movie) {
+        return client.reviews(movie.getId());
+    }
 }
